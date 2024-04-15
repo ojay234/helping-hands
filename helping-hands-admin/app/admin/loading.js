ff0e75fbeac58658e2773de22358056c2a4c267a
David Ochuma <davidace998@gmail.com>
2024-04-15 10:41:39 +0100

    updated loading ui

--- helping-hands-admin/app/admin/loading.js
+++ helping-hands-admin/app/admin/loading.js
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <p className="font-bold text-center text-2xl text-gray-400">Loading...</p>
+    </div>
+  );
 }
